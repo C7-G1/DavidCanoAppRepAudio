4c01e43041a352e35f769306f32e92201f08aef9
Draco32170 <canodavid498@gmail.com>
2022-01-20 13:57:29 +0100

    Funciona

--- app/src/main/java/david/cano/davidcanoapprepaudio/MainActivity.java
+++ app/src/main/java/david/cano/davidcanoapprepaudio/MainActivity.java
@@ -4,14 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp=new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MediaPlayer mp=MediaPlayer.create(this,R.raw.pisandofuerte);
+        mp=MediaPlayer.create(this,R.raw.stilllovingyou);
+
+    }
+
+    public void iniciar(View v){
+        try {
+            mp.prepare();
+            mp.start();
+
+        }catch (IOException ex){
+            System.out.println("El fichero de audio no existe"+ ex.getMessage());
+        }
+
+    }
+    public void reiniciar(View v){
         mp.start();
     }
+    public void pausar(View v){
+       mp.pause();
+    }
+    public void parar(View v){
+        mp.stop();
+    }
+
 }
